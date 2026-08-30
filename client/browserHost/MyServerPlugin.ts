c0057790252d35cc7d9550091b5cf765c6ad2ad2
toby <tobywisener@googlemail.com>
2026-08-30 09:11:01 +0100

    feat(host): configure default experience rates

--- client/browserHost/MyServerPlugin.ts
+++ client/browserHost/MyServerPlugin.ts
@@ -1,9 +1,23 @@
 export const DEFAULT_MY_SERVER_PLUGIN = `module.exports = {
+
   name: "MyServer",
+
+
   register(api) {
+
+    // Set combat and regular XP multipliers.
+    api.setExperienceRates({
+      combat: 6,
+      regular: 18,
+    });
+
+
+    // Send a greeting when a player logs in.
     api.onPlayerLogin(({ player }) => {
       player.getPacketSender().sendMessage("Hello world!");
     });
+
   },
+
 };
 `;
